5327684d87dfc2899310a9d8b1201589a4515b64
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-06-17 16:17:16 -0500

    fix warning

--- modules/core/home-manager.nix
+++ modules/core/home-manager.nix
@@ -2,6 +2,8 @@
   flake.modules.nixos.core_home-manager = { lib, username, ... }: {
     imports = [(lib.mkAliasOptionModule ["hm"] ["home-manager" "users" username])];
 
+    home-manager.backupFileExtension = "backup";
+
     hm = {
       home.username = username;
       home.homeDirectory = "/home/${username}";
