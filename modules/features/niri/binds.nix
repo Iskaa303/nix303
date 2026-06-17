2261c3cf8cd12574b8ac0adcf68c750fb1760a87
Iskaa303 <iska060808@gmail.com>
2026-06-16 20:17:19 -0400

    fix noctalia shell and add foot as a fallback terminal

--- modules/features/niri/binds.nix
+++ modules/features/niri/binds.nix
@@ -2,6 +2,7 @@
   flake.modules.nixos.feature_niri = {
     hm.programs.niri.settings.binds = {
       "Mod+Return".action.spawn = "ghostty";
+      "Mod+T".action.spawn = "foot";
       "Mod+Q".action.close-window = {};
     };
   };
--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,7 +1,10 @@
-{ ... }: {
-  flake.modules.nixos.feature_noctalia = { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-      noctalia-shell
-    ];
+{ inputs, ... }: {
+  flake.modules.nixos.feature_noctalia = {
+    services.upower.enable = true;
+
+    hm = {
+      imports = [inputs.noctalia.homeModules.default];
+      programs.noctalia-shell.enable = true;
+    };
   };
 }
