eda9605d7cd822cb0f1031f7ae605d9b3f2984e1
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-06-17 15:38:22 -0500

    added unholy antigravity temporarily because i have subscription

---
+++ modules/features/antigravity.nix
@@ -0,0 +1,15 @@
+{ inputs, ... }: {
+  flake.modules.nixos.feature_antigravity = {
+   # nixpkgs.config.allowUnfreePredicate = pkg: 
+   #   builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+   #     "google-antigravity"
+   #     "google-antigravity-ide"
+   #   ];
+
+    hm = {
+      home.packages = [
+        inputs.antigravity-nix.packages.${pkgs.system}.default
+      ];
+    };
+  };
+}
