253fedafae5da7a0799a1f75bf6de336b05e8730
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-06-20 10:18:56 -0400

    fixed devenv shell not working

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -2,6 +2,11 @@
   flake.modules.nixos.core_base = { pkgs, ... }: {
     nixpkgs.config.allowUnfree = true;
 
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "iskaa303" ];
+    };
+
     environment.systemPackages = with pkgs; [
       curl
       bash
