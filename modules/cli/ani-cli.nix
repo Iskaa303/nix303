dd32219d6c3166e9232e6a07a6b6af7985dcc57d
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-07-08 16:15:41 -0400

    added ani-skip

--- modules/cli/ani-cli.nix
+++ modules/cli/ani-cli.nix
@@ -6,8 +6,22 @@
           owner = "pystardust";
           repo = "ani-cli";
           rev = "master";
-          hash = "sha256-+fR46bWXJ58LkXFvWAO/LyCd5THi7oMcqmhRoCKBZfM=";
+          hash = "sha256-D7pWbjmhCLAbtoUBV4KtqbjusvjFd0mLCgFh+Pjp5SY=";
         };
+      }))
+      (pkgs.ani-skip.overrideAttrs (old: {
+        src = pkgs.fetchFromGitHub {
+          owner = "synacktraa";
+          repo = "ani-skip";
+          rev = "master";
+          hash = lib.fakeHash;
+        };
+        installPhase = ''
+          runHook preInstall
+          install -D integrations/mpv.lua $out/share/mpv/scripts/skip.lua
+          install -Dm 755 ani-skip $out/bin/ani-skip
+          runHook postInstall
+        '';
       }))
     ];
   };
