94fcda093d4ba2f065506e99fe386664543a9787
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-07-08 16:38:01 -0400

    update hashes and revs for ani-cli and ani-skip

--- modules/cli/ani-cli.nix
+++ modules/cli/ani-cli.nix
@@ -1,20 +1,20 @@
 { ... }: {
-  flake.modules.nixos.cli_anime = { pkgs, lib, ... }: {
+  flake.modules.nixos.cli_anime = { pkgs, ... }: {
     hm.home.packages = [
       (pkgs.ani-cli.overrideAttrs (old: {
         src = pkgs.fetchFromGitHub {
           owner = "pystardust";
           repo = "ani-cli";
-          rev = "master";
-          hash = "sha256-D7pWbjmhCLAbtoUBV4KtqbjusvjFd0mLCgFh+Pjp5SY=";
+          rev = "89ec9314eba7b66c894bf8bbf3b2b25a3b80743a";
+          hash = "sha256-wU25uSikLbuzQ/nAZzWz3ilpM1Ewac4ZuICRCwUn/fQ=";
         };
       }))
       (pkgs.ani-skip.overrideAttrs (old: {
         src = pkgs.fetchFromGitHub {
           owner = "synacktraa";
           repo = "ani-skip";
-          rev = "master";
-          hash = lib.fakeHash;
+          rev = "6890bbfcc3f1dc785cfca9fede10cc9152f03161";
+          hash = "sha256-D7pWbjmhCLAbtoUBV4KtqbjusvjFd0mLCgFh+Pjp5SY=";
         };
         installPhase = ''
           runHook preInstall
