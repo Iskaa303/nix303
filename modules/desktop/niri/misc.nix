15adb7a27a56da44655bbbbbc5404579bf5de3b0
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-08-17 22:24:30 -0400

    round corners niri

--- modules/desktop/niri/misc.nix
+++ modules/desktop/niri/misc.nix
@@ -44,6 +44,16 @@
 
       window-rules = [
         {
+          # Rounded corners for window + focus ring, no sharp blue corners.
+          geometry-corner-radius = {
+            top-left = 12.0;
+            top-right = 12.0;
+            bottom-right = 12.0;
+            bottom-left = 12.0;
+          };
+          clip-to-geometry = true;
+        }
+        {
           matches = [
             { app-id = "^mpv$"; }
           ];
